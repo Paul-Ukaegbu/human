701cd4311381cdd8ce99d4a0d914384615838e5c
paul-py07 <pauliukaegbu@gmail.com>
2020-04-13 06:14:44 -0500

    commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace human
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Human{
+        // Fields for Human
+        public string Name;
+        public int Strength;
+        public int Intelligence;
+        public int Dexterity;
+        private int health{
+            get {return health;}
+        }
+
+         
+        // add a public "getter" property to access health
+         
+        // Add a constructor that takes a value to set Name, and set the remaining fields to default values
+         
+        // Add a constructor to assign custom values to all fields
+         
+        // Build Attack method
+        public int Attack(Human target){
+            
+        }
+
+}
+
+}
